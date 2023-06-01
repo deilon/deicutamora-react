d019ddc93c435e5230215b35bb7bf57f800d107f
Deilon <deilast80@gmail.com>
2023-06-01 16:49:12 +0800

    Update certificate titles

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -7,21 +7,21 @@ const Certificates = () => {
             {/* KodeGO */}
             <div className="container mx-auto pt-12 py-5 px-6">
                 <div className="w-full mb-15 lg:w-5/12">
-                    <h1 className="text-xl text-deepBlue mb-5 font-semibold">KodeGO Certificate of Achievement</h1>
+                    <h1 className="text-xl text-deepBlue mb-5 font-semibold">KodeGO award</h1>
                     <img src="assets/certificates/deilon5star.png" className="mb-5 w-full"></img>
                 </div>
             </div>
             {/* ICT week participation */}
             <div className="container mx-auto pt-12 py-5 px-6">
                 <div className="w-full mb-15 lg:w-5/12">
-                    <h1 className="text-xl text-deepBlue mb-5 font-semibold">ICT Week Certificate of Appreciation</h1>
+                    <h1 className="text-xl text-deepBlue mb-5 font-semibold">ICT Week Participation</h1>
                     <img src="assets/certificates/participation.png" className="mb-5 w-full"></img>
                 </div>
             </div>
             {/* BSIS1 */}
             <div className="container mx-auto pt-12 py-5 px-6">
                 <div className="w-full mb-15 lg:w-5/12">
-                    <h1 className="text-xl text-deepBlue mb-5 font-semibold">Certificate of Recognition, Hackathon</h1>
+                    <h1 className="text-xl text-deepBlue mb-5 font-semibold">Hackathon</h1>
                     <img src="assets/certificates/bsis1-hackathon.png" className="mb-5 w-full"></img>
                 </div>
             </div>
