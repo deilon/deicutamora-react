365b2d2a9c47b10dfba25a94bb013808fceda687
Deilon <deilast80@gmail.com>
2023-06-11 13:52:57 +0800

    Edit cert title

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -14,7 +14,7 @@ const Certificates = () => {
             {/* KodeGO Cert of Completion */}
             <div className="container mx-auto pt-12 py-5 px-6">
                 <div className="w-full mb-15 lg:w-5/12">
-                    <h1 className="text-xl text-deepBlue mb-5 font-semibold">KodeGO award</h1>
+                    <h1 className="text-xl text-deepBlue mb-5 font-semibold">KodeGO Certificate of Completion</h1>
                     <img src="assets/certificates/cert_completion.jpg" className="mb-5 w-full"></img>
                 </div>
             </div>
